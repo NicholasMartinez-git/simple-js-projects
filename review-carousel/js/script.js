8b3fc385390aadbd30ed70b156de616b1247ea0d
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-08-02 16:07:26 -0500

    feat: Create js mock-up
    There were also some updates to the html mock-up

--- review-carousel/js/script.js
+++ review-carousel/js/script.js
@@ -1,1 +1,12 @@
-/* JS GOES HERE*/
+let users = [
+    {
+        firstName: 'John',
+        lastName: 'Smith',
+        fullName: function () {
+            return this.firstName + " " + this.lastName;
+        },
+        imageFile: 'pexels-john-smith.jpg',
+        occupation: 'Web Developer',
+        quote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.'
+    }
+]
